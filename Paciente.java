b38f89c7a7005cab7ac46f8e93592dc49cca068b
RManriquesu <rmanriquesu@unsa.edu.pe>
2025-10-15 20:00:01 -0500

    Implementacion de la clase Paciente

---
+++ Paciente.java
@@ -0,0 +1,33 @@
+public class Paciente {
+    private String codigo;
+    private String nombre;
+    private int edad;
+    private String documento;
+    
+    //Metodo constructorr
+    public Paciente(String codigo, String nombre, int edad, String documento) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.edad = edad;
+        this.documento = documento;
+    }
+
+    public String getCodigo() { 
+    return codigo; }
+
+    public String getNombre() { 
+    return nombre; }
+
+    public int getEdad() {
+    return edad; }
+
+    public String getDocumento() { 
+    return documento; }
+
+    @Override
+    public String toString() {
+    return "Paciente{" + "codigo='" + codigo + '\'' + ", nombre='" + nombre + '\'' +
+    ",edad=" + edad + ", documento='" + documento + '\'' + '}';
+    }
+}
+
